d961f91f65a52a345739ae9540a087fbc00f9ec4
Max Kroft <kroftmaxwell@gmail.com>
2026-01-15 15:27:02 -0600

    test to make sure _rvModel is compiled and works

---
+++ tests/test_rvModel.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from alfred._rv_func import _rvModel
+
+
+def test_rvModel():
+
+    par = np.array([5.56, 100, 11, 0.23, 1.2], dtype = float)
+    t = np.array([99,100,101], dtype = float)
+
+    rv = _rvModel(par, t)
+
+    assert np.isclose(rv, np.array([11.82788683, 0.91676512, -10.00130897]), atol = 1e-5).all()
