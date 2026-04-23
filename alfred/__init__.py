7f8e9194159260ca0293763ea815f7670e494bb3
maxkroft <103783561+maxkroft@users.noreply.github.com>
2026-04-23 15:24:48 -0500

    Update __init__.py to 0.99.4 for second release test

--- alfred/__init__.py
+++ alfred/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.99.3'
+__version__ = '0.99.4'
 
 ld_grid_dir = os.path.dirname(os.path.realpath(__file__))+'/ld_grids/'
 data_dir = os.path.dirname(os.path.realpath(__file__))+'/example_data/'
