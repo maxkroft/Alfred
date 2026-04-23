b95fa1f41a8210495820335bbba5ac2438f21416
Max Kroft <kroftmaxwell@gmail.com>
2026-04-23 14:45:40 -0500

    Changing project name to Alfred-Exoplanets, as alfred is taken on pypi

--- alfred/__init__.py
+++ alfred/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.99.0'
+__version__ = '0.99.2'
 
 ld_grid_dir = os.path.dirname(os.path.realpath(__file__))+'/ld_grids/'
 data_dir = os.path.dirname(os.path.realpath(__file__))+'/example_data/'
