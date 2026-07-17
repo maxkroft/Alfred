81f290b06242d29f936d994d72fb58f2bc196209
Max Kroft <kroftmaxwell@gmail.com>
2026-07-17 12:10:25 -0500

    init_planets gui done

--- alfred/init_gui.py
+++ alfred/init_gui.py
@@ -52,6 +52,12 @@ class FloatEntry(ctk.CTkFrame):
             return False
 
 
+    def return_float(self):
+        try:
+            return float(self.entry.get().strip())
+        except ValueError:
+            return np.nan
+
     
 class ToggleCheckbox(ctk.CTkCheckBox):
     def __init__(self, master, label, startval=0, toggles=[]):
@@ -75,12 +81,15 @@ class ToggleCheckbox(ctk.CTkCheckBox):
 
             for x in self.toggles:
                 x.grid_forget()
+
+    def return_bool(self):
+        return self.val.get() == 1
     
 
 
 
-class PlanetGUI(ctk.CTk):
-    def __init__(self, data: Row = None):
+class PlanetGUI(ctk.CTkToplevel):
+    def __init__(self, data: Row):
         super().__init__()
 
         self.data = data
@@ -90,42 +99,33 @@ class PlanetGUI(ctk.CTk):
         self.grid_columnconfigure((0,1), weight = 1)
 
         self.p_entry = FloatEntry(self, "P (days)", 1, 0, min_val = 0)
-        if self.data is not None:
-            self.p_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Period']) else self.data['Period']))
+        self.p_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Period']) else self.data['Period']))
         self.p_entry.grid(row = self.p_entry.row, column = self.p_entry.col, sticky = "ew", padx = 10)
 
         self.t_entry = FloatEntry(self, "Tc (BJD-2450000)", 1, 1)
-        if self.data is not None:
-            self.t_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Tc']) else self.data['Tc']))
+        self.t_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Tc']) else self.data['Tc']))
         self.t_entry.grid(row = self.t_entry.row, column = self.t_entry.col, sticky = "ew", padx = 10)
 
         self.r_entry = FloatEntry(self, "Rp/Rstar", 2, 0, min_val = 0)
-        if self.data is not None:
-            self.r_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Rp/Rs']) else self.data['Rp/Rs']))
+        self.r_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['Rp/Rs']) else self.data['Rp/Rs']))
 
         self.a_entry = FloatEntry(self, "a/Rstar", 2, 1, min_val = 0)
-        if self.data is not None:
-            self.a_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['a/Rs']) else self.data['a/Rs']))
+        self.a_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['a/Rs']) else self.data['a/Rs']))
 
         self.i_entry = FloatEntry(self, "cos(i)", 3, 0, min_val = 0, max_val = 1)
-        if self.data is not None:
-            self.i_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['cos(i)']) else self.data['cos(i)']))
+        self.i_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['cos(i)']) else self.data['cos(i)']))
 
         self.k_entry = FloatEntry(self, "K (m/s)", 3, 1, min_val = 0)
-        if self.data is not None:
-            self.k_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['K']) else self.data['K']))
+        self.k_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['K']) else self.data['K']))
 
         self.secw_entry = FloatEntry(self, "sqrt(e)cos(w) (-1 to 1)", 4, 0, min_val = -1, max_val = 1)
-        if self.data is not None:
-            self.secw_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['sqrt(e)cos(w)']) else self.data['sqrt(e)cos(w)']))
+        self.secw_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['sqrt(e)cos(w)']) else self.data['sqrt(e)cos(w)']))
 
         self.sesw_entry = FloatEntry(self, "sqrt(e)sin(w) (-1 to 1)", 4, 1, min_val = -1, max_val = 1)
-        if self.data is not None:
-            self.sesw_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['sqrt(e)sin(w)']) else self.data['sqrt(e)sin(w)']))
+        self.sesw_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['sqrt(e)sin(w)']) else self.data['sqrt(e)sin(w)']))
 
         self.f_entry = FloatEntry(self, "Fp/Fstar", 5, 0, min_val = 0)
-        if self.data is not None:
-            self.f_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['fp']) else self.data['fp']))
+        self.f_entry.entry.configure(textvariable = ctk.StringVar(value = '' if np.isnan(self.data['fp']) else self.data['fp']))
 
 
         self.checkbox_frame = ctk.CTkFrame(self)
@@ -148,6 +148,175 @@ class PlanetGUI(ctk.CTk):
         self.se_check.grid(row = 4, column = 0, padx=10, pady=(10, 0), sticky="w")
 
 
-tab = Table.read('/mnt/c/Users/kroft/Documents/Data/7690/init_planets.txt', format = 'ascii.fixed_width_two_line', header_rows = ['name','unit'], delimiter = '|', converters = {'*': [int, float, bool, str]})
-app = PlanetGUI(data = tab[0])
+        self.save = ctk.CTkButton(self, text = 'Save', command = self.save_cmd, font = ('Arial', 18, 'bold'))
+        self.save.grid(row = 6, column = 0, padx = 10, pady = (10,0), sticky = "ew", columnspan = 2)
+
+
+
+    def save_cmd(self):
+
+        self.data['Transiting'] = self.t_check.return_bool()
+        self.data['RV Signal'] = self.r_check.return_bool()
+        self.data['Fit Ecc'] = self.e_check.return_bool()
+        self.data['Fit TTVs'] = self.ttv_check.return_bool()
+        self.data['Fit Eclipse'] = self.se_check.return_bool()
+        self.data['Period'] = self.p_entry.return_float()
+        self.data['Tc'] = self.t_entry.return_float()
+        self.data['Rp/Rs'] = self.r_entry.return_float()
+        self.data['a/Rs'] = self.a_entry.return_float()
+        self.data['cos(i)'] = self.i_entry.return_float()
+        self.data['K'] = self.k_entry.return_float()
+        self.data['sqrt(e)cos(w)'] = self.secw_entry.return_float()
+        self.data['sqrt(e)sin(w)'] = self.sesw_entry.return_float()
+        self.data['fp'] = self.f_entry.return_float()
+
+        self.destroy()
+
+
+class DeletePrompt(ctk.CTkToplevel):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry('300x100')
+        self.grid_columnconfigure((0,1), weight = 1)
+        self.grid_rowconfigure((0,1), weight = 1)
+
+        self.answer = False
+
+        self.label = ctk.CTkLabel(self, text = 'Delete?', font = ('Arial', 18, 'bold'))
+        self.label.grid(row = 0, column = 0, padx = 10, sticky = 'ew', columnspan = 2)
+
+        self.yes = ctk.CTkButton(self, text = 'Yes', command = self.yes_cmd, font = ('Arial', 18, 'bold'))
+        self.yes.grid(row = 1, column  = 0, padx = 10, sticky = 'ew')
+
+        self.no = ctk.CTkButton(self, text = 'No', command = self.no_cmd, font = ('Arial', 18, 'bold'))
+        self.no.grid(row = 1, column = 1, padx = 10, sticky = 'ew')
+
+    def yes_cmd(self):
+
+        self.answer = True
+
+        self.destroy()
+
+    def no_cmd(self):
+
+        self.destroy()
+
+
+class PlanetFrame(ctk.CTkFrame):
+    def __init__(self, master, topmaster, data: Row, idx):
+        super().__init__(master)
+
+        self.grid_columnconfigure(list(range(1,4)), weight = 1)
+
+        self.data = data
+        self.topmaster = topmaster
+        self.idx = idx
+
+        self.labelvar = ctk.StringVar(value = '{0}. P = {1:.2f}'.format(self.idx+1, self.data['Period']))
+        self.label = ctk.CTkLabel(self, textvariable = self.labelvar, font = ('Arial', 18, 'bold'))
+        self.label.grid(row = 0, column = 0, padx = 10, sticky = 'ew')
+
+        self.edit = ctk.CTkButton(self, text = 'Edit', command = self.edit_cmd, font = ('Arial', 18, 'bold'))
+        self.edit.grid(row = 0, column = 1, padx = 10, sticky = 'ew')
+
+        self.copy = ctk.CTkButton(self, text = 'Copy', command = lambda d=self.data: self.topmaster.copy_cmd(d), font = ('Arial', 18, 'bold'))
+        self.copy.grid(row = 0, column = 2, padx = 10, sticky = 'ew')
+
+        self.delete = ctk.CTkButton(self, text = 'Delete', command = self.delete_cmd, font = ('Arial', 18, 'bold'))
+        self.delete.grid(row = 0, column = 3, padx = 10, sticky = 'ew')
+
+    def edit_cmd(self):
+
+        edit_planet = PlanetGUI(self.data)
+
+        self.wait_window(edit_planet)
+
+        self.data = edit_planet.data
+
+        self.update_label()
+
+    def delete_cmd(self):
+
+        self.topmaster.delete_cmd(self.idx)
+
+    def update_label(self):
+
+        self.labelvar.set('{0}. P = {1:.2f}'.format(self.idx+1, self.data['Period']))
+
+
+class InitPlanetGUI(ctk.CTk):
+    def __init__(self, table: Table):
+        super().__init__()
+
+        self.table = table
+
+        self.geometry('800x600')
+        self.grid_columnconfigure((0,1), weight = 1)
+
+        self.planets_frame = ctk.CTkScrollableFrame(self)
+        self.planets_frame.grid(row = 0, column = 0, padx = 10, pady = (10,0), sticky = 'nesw', columnspan = 2)
+        self.planets_frame.grid_columnconfigure(0, weight = 1)
+
+        self.frame_title = ctk.CTkLabel(self.planets_frame, text = 'Planets', font = ('Arial', 18, 'bold'))
+        self.frame_title.grid(row = 0, column = 0, padx = 10, sticky = 'ew')
+
+        self.planets = []
+
+        for i in range(len(self.table)):
+
+            planet = PlanetFrame(self.planets_frame, self, table[i], i)
+            planet.grid(row = i+1, column = 0, pady = (10, 0), sticky = 'ew')
+            self.planets.append(planet)
+
+        self.add = ctk.CTkButton(self, text = 'Add', command = self.add_cmd, font = ('Arial', 18, 'bold'))
+        self.add.grid(row = 1, column = 0, padx = 10, pady = (10, 0), sticky = 'ew')
+
+        self.save = ctk.CTkButton(self, text = 'Save', command = self.save_cmd, font = ('Arial', 18, 'bold'))
+        self.save.grid(row = 1, column = 1, padx = 10, pady = (10, 0), sticky = 'ew')
+
+    def add_cmd(self):
+        
+        self.table.add_row([False]*5 + [np.nan]*9)
+        i = len(self.planets)
+        planet = PlanetFrame(self.planets_frame, self, self.table[i], i)
+        planet.grid(row = i+1, column = 0, pady = (10,0), sticky = 'ew')
+        self.planets.append(planet)
+        planet.edit_cmd()
+
+    def save_cmd(self):
+        
+        self.destroy()
+
+    def copy_cmd(self, data: Row):
+
+        self.table.add_row(data)
+        i = len(self.planets)
+        planet = PlanetFrame(self.planets_frame, self, self.table[i], i)
+        planet.grid(row = i+1, column = 0, pady = (10,0), sticky = 'ew')
+        self.planets.append(planet)
+
+
+    def delete_cmd(self, i):
+
+        delete_prompt = DeletePrompt()
+
+        self.wait_window(delete_prompt)
+
+        if delete_prompt.answer:
+
+            self.table.remove_row(i)
+            self.planets[i].grid_forget()
+            self.planets.pop(i)
+
+            for j in range(len(self.planets)):
+                self.planets[j].grid(row = j+1, column = 0, pady = (10, 0), sticky = 'ew')
+                self.planets[j].idx = j
+                self.planets[j].data = self.table[j]
+                self.planets[j].update_label()
+
+
+tab = Table.read('/mnt/c/Users/kroft/Documents/Data/7178/init_planets2.txt', format = 'ascii.fixed_width_two_line', header_rows = ['name','unit'], delimiter = '|', converters = {'*': [int, float, bool, str]})
+# app = PlanetGUI(data = tab[0])
+app = InitPlanetGUI(tab)
 app.mainloop()
